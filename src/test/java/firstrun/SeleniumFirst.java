8e134cc229e5b5f69761cdfacfd31d9087a11bc0
MalikCmoi <malik.tcheboun@gmail.com>
2023-11-24 19:43:11 +0100

    Amélirations du code

--- src/test/java/firstrun/SeleniumFirst.java
+++ src/test/java/firstrun/SeleniumFirst.java
@@ -1,7 +1,11 @@
 package firstrun;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 public class SeleniumFirst {
     public static void main(String[] args) {
@@ -9,6 +13,11 @@ public class SeleniumFirst {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
         System.out.println(driver.getTitle());
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+        textBox.sendKeys("Selenium");
+        submitButton.click();
         driver.quit();
     }
 }
